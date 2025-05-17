cd81bcc7cfead036ece773bd55cc0aa972ae1d3d
Jesus David Perez <jesusdpp96@gmail.com>
2025-05-17 23:41:08 +0000

    chore: make some necesary configurations to make building process work

--- server/src/apps/express-server/server.ts
+++ server/src/apps/express-server/server.ts
@@ -3,11 +3,6 @@ import { config } from './config'
 
 const PORT = config.port
 
-console.log(`\n\n-----------------------------------`)
-console.warn(`¡Do not leave secrets in logs!\n`)
-console.log(`Server configuration: ${JSON.stringify(config, null, 2)}`)
-console.log(`-----------------------------------\n\n`)
-
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
 })
