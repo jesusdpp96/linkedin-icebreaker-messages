486115a4289fef5dbb0933817e189e39038f5cb6
Jesus David Perez <jesusdpp96@gmail.com>
2025-06-01 21:37:24 +0000

    refactor(services): openai service Make things more simple and adjust to the new error handler

--- server/src/services/openai-api/types/chat-request-options.ts
+++ server/src/services/openai-api/types/chat-request-options.ts
@@ -4,10 +4,13 @@
 export interface ChatRequestOptions {
   /** The main prompt as a string. */
   prompt: string
-
   /**
    * Optional array of JSON inputs, already stringified.
    * These will be appended to the prompt contextually.
    */
   jsonInputs?: string[]
+  /**
+   * TODO: describe
+   */
+  structuredResponseSchema?: string
 }
--- server/src/services/openai-api/types/openai-config.ts
+++ server/src/services/openai-api/types/openai-config.ts
@@ -5,9 +5,6 @@ export interface OpenAIConfig {
   /** OpenAI API key */
   apiKey: string
 
-  /** Optional base URL for OpenAI API. Defaults to https://api.openai.com/v1 */
-  baseURL?: string
-
   /** Model to use, e.g., "gpt-4", "gpt-3.5-turbo" */
   model?: string
 
